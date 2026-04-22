8e746c05f57314164bb85ccda813653cd7c2df33
Sisou <fabienkiefer24@gmail.com>
2026-04-22 18:46:01 +0200

    gitignore: add .gitignore

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import axios from "axios";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const CLIENT_ID = process.env.CLIENT_ID;
